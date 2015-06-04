11197ca5417ec48b3e14677ddca8edafe662dccf
Titus Iudean <iudeant@gmail.com>
2015-06-04 16:24:29 +0100

    Finished chapter 2

---
+++ ch2/ex5.rb
@@ -0,0 +1,1 @@
+# the first program outputs 3 but the second gives an error as x is a local variable defined within the do-end block and it is not visible outside that block
---
+++ ch2/ex6.rb
@@ -0,0 +1,1 @@
+# the error message indicates the variable 'shoes' has either not been defined before it was used or it was defined within a block and it is not visible where it was used
